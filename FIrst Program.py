eddf5a03273304921e0ede778ee9366c69d34911
reddevil7291 <sbap7291@gmail.com>
2018-10-28 15:29:30 +0530

    FIrst Program

---
+++ FIrst Program.py
@@ -0,0 +1,5 @@
+print('Hello')
+Principal = input("Enter the Principal Amount :")
+Rate : a
+print(name)
+Arun
---
+++ SImple Interest Problem.py
@@ -0,0 +1,9 @@
+print("Hello")
+a = float(input("Enter the Principal Amount:"))
+b = float(input("\nEnter the rate of Interest: "))
+c = float(input("\nEnter the Time Period:"))
+si = float
+
+si = (a*b*c)/100
+
+print("\nThe Simple interest is",si)
